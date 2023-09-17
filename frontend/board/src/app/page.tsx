2f25c1b45adfc579d13355b9cfffd3cbe2d02783
MACBOOK <natachigram@gmail.com>
2023-09-17 11:45:38 +0100

    submit

--- frontend/board/src/app/page.tsx
+++ frontend/board/src/app/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 import { useState, ChangeEvent } from 'react';
-// import '@rainbow-me/rainbowkit/styles.css';
+import { ethers } from 'ethers';
 
 export default function Home() {
+
+  const provider = ethers.getDefaultProvider("sepolia");
+  
+  //----------------------------------------------------------
+  
   const [inputValue1, setInputValue1] = useState<string>('');
   const [inputValue2, setInputValue2] = useState<string>('');
 
@@ -42,64 +47,64 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <main className='flex items-center min-h-screen flex-row  min-w-fit bg-gradient-to-r from-colorRight to-colorLeft'>
-        <div className=' basis-2/4 right-section h-full p-12'>
-          <h1 className='font-primary font-bold text-9xl'>Board Dapp</h1>
-          <p className='text-xl font-light mt-4'>
-            Enter Values to locate your box. Row values should not exceed 5 and
-            column values should exceed 7 👍 🦄
-          </p>
-          <form className='flex flex-col mt-14'>
-            <div className='inputs flex flex-row items-center gap-4'>
-              <input
-                placeholder='Row'
-                type='number'
-                className='cursor-pointer basis-2/4 w-28 h-14 text-2xl text-center border-2 rounded-md text-[white]'
-                // max={5}
-                // min={1}
-                value={inputValue1}
-                onChange={handleInputChange1}
-                maxLength={maxLength1}
-              />
-              <input
-                // min={1}
-                // max={7}
-                value={inputValue2}
-                onChange={handleInputChange2}
-                maxLength={maxLength2}
-                placeholder='Column'
-                type='number'
-                className='cursor-pointer basis-2/4 w-28 h-14 text-2xl text-center border-2 rounded-md text-[white]'
-              />
+        <div>
+          <main className='flex items-center min-h-screen flex-row  min-w-fit bg-gradient-to-r from-colorRight to-colorLeft'>
+            <div className=' basis-2/4 right-section h-full p-12'>
+              <h1 className='font-primary font-bold text-9xl'>Board Dapp</h1>
+              <p className='text-xl font-light mt-4'>
+                Enter Values to locate your box. Row values should not exceed 5
+                and column values should exceed 7 👍 🦄
+              </p>
+              <form className='flex flex-col mt-14'>
+                <div className='inputs flex flex-row items-center gap-4'>
+                  <input
+                    placeholder='Row'
+                    type='number'
+                    className='cursor-pointer basis-2/4 w-28 h-14 text-2xl text-center border-2 rounded-md text-[white]'
+                    // max={5}
+                    // min={1}
+                    value={inputValue1}
+                    onChange={handleInputChange1}
+                    maxLength={maxLength1}
+                  />
+                  <input
+                    // min={1}
+                    // max={7}
+                    value={inputValue2}
+                    onChange={handleInputChange2}
+                    maxLength={maxLength2}
+                    placeholder='Column'
+                    type='number'
+                    className='cursor-pointer basis-2/4 w-28 h-14 text-2xl text-center border-2 rounded-md text-[white]'
+                  />
+                </div>
+
+                <button
+                  type='submit'
+                  className='font-bold cursor-pointer h-14 bg-colorLeft w-full mt-4 rounded-md'
+                >
+                  Locate Box
+                </button>
+              </form>
             </div>
 
-            <button
-              type='submit'
-              className='font-bold cursor-pointer h-14 bg-colorLeft w-full mt-4 rounded-md'
-            >
-              Locate Box
-            </button>
-          </form>
+            <div className=' basis-2/4 left-section p-12 '>
+              <div className='container h-full'>{divs}</div>
+            </div>
+          </main>
+          <div className='min-h-screen min-w-fit fconnect-wallet bg-gradient-to-r from-colorRight to-colorLeft'>
+            <h1 className='font-primary font-bold text-9xl flex justify-center items-center'>
+              Board Dapp
+            </h1>
+            <form className='flex w-full justify-center items-center'>
+              <button
+                type='submit'
+                className='font-bold cursor-pointer h-14 bg-colorLeft w-2/5 mt-4 rounded-md'
+              >
+                Connect Wallet To Continue
+              </button>
+            </form>
+          </div>
         </div>
-
-        <div className=' basis-2/4 left-section p-12 '>
-          <div className='container h-full'>{divs}</div>
-        </div>
-      </main>
-      <div className='min-h-screen min-w-fit fconnect-wallet bg-gradient-to-r from-colorRight to-colorLeft'>
-        <h1 className='font-primary font-bold text-9xl flex justify-center items-center'>
-          Board Dapp
-        </h1>
-        <form className='flex w-full justify-center items-center'>
-          <button
-            type='submit'
-            className='font-bold cursor-pointer h-14 bg-colorLeft w-2/5 mt-4 rounded-md'
-          >
-            Connect Wallet To Continue
-          </button>
-        </form>
-      </div>
-    </div>
   );
 }
